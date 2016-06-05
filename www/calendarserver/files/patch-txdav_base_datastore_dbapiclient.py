c4ff56212ac91ebcf07717a661c40c3658ce8447
Axel Rau <axel.rau@chaos1.de>
2016-06-05 17:17:38 +0000

    Fix previous commit

--- www/calendarserver/files/patch-txdav_base_datastore_dbapiclient.py
+++ www/calendarserver/files/patch-txdav_base_datastore_dbapiclient.py
@@ -1,10 +1,10 @@
---- txdav/base/datastore/dbapiclient.py.orig	2016-06-05 15:42:24 UTC
+--- txdav/base/datastore/dbapiclient.py.orig	2016-03-01 19:40:29 UTC
 +++ txdav/base/datastore/dbapiclient.py
 @@ -427,7 +427,10 @@ def pg8000Preflight(connection, **kwargs
          return v.encode("utf-8") if isinstance(v, unicode) else str(v)
  
      connection.realConnection.py_types[str] = (705, postgres.core.FC_TEXT, my_text_out)
--    connection.realConnection.py_types[postgres.text_type] = (705, postgres.core.FC_TEXT, my_text_out)
+-    connection.realConnection.py_types[postgres.six.text_type] = (705, postgres.core.FC_TEXT, my_text_out)
 +
 +    # http://trac.calendarserver.org/ticket/940
 +    from six import text_type
