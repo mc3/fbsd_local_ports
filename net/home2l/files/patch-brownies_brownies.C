1f5f05b22adcfdc3ded2cdb9bcbc4ce428f58b33
ajr <axel.rau@chaos1.de>
2022-03-30 17:19:49 +0000

    Hacked socket credentils in brownies.C

--- net/home2l/files/patch-brownies_brownies.C
+++ net/home2l/files/patch-brownies_brownies.C
@@ -1,14 +1,34 @@
---- brownies/brownies.C.orig	2022-03-29 11:29:01 UTC
+--- brownies/brownies.C.orig	2021-12-10 19:40:51 UTC
 +++ brownies/brownies.C
-@@ -29,8 +29,9 @@
+@@ -29,7 +29,8 @@
  #include <stdarg.h>
  #include <sys/socket.h>   // for socket server ...
  #include <sys/un.h>
 -#include <linux/i2c-dev.h>
--
-+//#include <linux/i2c-dev.h>
-+// Does not work:
-+#include i2c-dev.h
++#include "i2c-dev.h"
++//#include <dev/iicbus/iic.h>
  
  
  
+@@ -1060,13 +1061,14 @@ static int SocketServerAccept (int listenFd, const cha
+   }
+ 
+   // On success: Try to get peer credentials and log connection ...
+-  if (clientFd >= 0 && name) {
+-    len = sizeof (struct ucred);
+-    if (getsockopt (clientFd, SOL_SOCKET, SO_PEERCRED, &ucred, &len) != 0)
+-      INFOF (("%s: Connection established from unkown client (failed to get peer credentials)", name));
+-    else
+-      INFOF (("%s: Connection established from (PID=%i, UID=%i, GID=%i)", name, ucred.pid, ucred.uid, ucred.gid));
+-  }
++// Not avalable on FreeBSD
++//  if (clientFd >= 0 && name) {
++//    len = sizeof (struct ucred);
++//    if (getsockopt (clientFd, SOL_SOCKET, SO_PEERCRED, &ucred, &len) != 0)
++//      INFOF (("%s: Connection established from unkown client (failed to get peer credentials)", name));
++//    else
++//      INFOF (("%s: Connection established from (PID=%i, UID=%i, GID=%i)", name, ucred.pid, ucred.uid, ucred.gid));
++//  }
+ 
+   // Done ...
+   return clientFd;
