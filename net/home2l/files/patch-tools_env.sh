76e7f0df6bb877f063393b6297ae1a9a4a4fb40f
ajr <axel.rau@chaos1.de>
2022-04-25 15:23:07 +0000

    Completed shebang-fixes + smaller improvements

--- net/home2l/files/patch-tools_env.sh
+++ net/home2l/files/patch-tools_env.sh
@@ -1,11 +1,24 @@
---- tools/env.sh.orig	2022-03-23 20:32:07 UTC
+--- tools/env.sh.orig	2022-04-21 17:30:43 UTC
 +++ tools/env.sh
-@@ -7,7 +7,7 @@
- export HOME2L_ROOT=`realpath \`dirname $BASH_SOURCE\``
+@@ -1,16 +1,18 @@
++#!/usr/local/bin/bash
++
+ # This file is part of the Home2L project.
+ #
+ # (C) 2015-2021 Gundolf Kiefer
+ 
+ 
+ # HOME2L_ROOT ...
+-export HOME2L_ROOT=`realpath \`dirname $BASH_SOURCE\``
++export HOME2L_ROOT="$(LOCALBASE)/home2l"
  
  # Architecture (for arch-dependent paths)...
 -export HOME2L_ARCH=`dpkg --print-architecture`
-+export HOME2L_ARCH=$(shell uname -p)
++export HOME2L_ARCH=`uname -p`
  
  # HOME2L_ETC (if not preset) ...
- [[ "$HOME2L_ETC" != "" ]] || export HOME2L_ETC=$HOME2L_ROOT/etc
+-[[ "$HOME2L_ETC" != "" ]] || export HOME2L_ETC=$HOME2L_ROOT/etc
++export HOME2L_ETC=$HOME2L_ROOT/etc
+ 
+ # Search and library paths...
+ export PATH=$HOME2L_ETC:$HOME2L_ROOT/bin:$HOME2L_ROOT/bin/$HOME2L_ARCH:$PATH
