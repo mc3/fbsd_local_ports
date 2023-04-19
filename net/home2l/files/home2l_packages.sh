5eeb261ad01c8787adc776b1f7e6a0c71ac78b86
ajr <axel.rau@chaos1.de>
2023-04-19 15:22:35 +0000

    Fixing "libinkscape_base.so: Undefined symbol" in home2l-1.2.2

--- net/home2l/files/home2l_packages.sh
+++ net/home2l/files/home2l_packages.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 echo "In home2l_packages"
-pkg install -y -r FreeBSD devel/git net/mosquitto shells/bash \
+# use devel/glib20 as workaround for ld-elf.so.1: /usr/local/bin/../lib/inkscape/
+#   libinkscape_base.so: Undefined symbol "g_string_free_and_steal"
+pkg install -y -r FreeBSD devel/git devel/glib20 net/mosquitto shells/bash \
         avr-binutils devel/avr-libc avr-gcc devel/avrdude devel/gettext-tools \
         devel/cmake devel/gmake \
         graphics/inkscape graphics/optipng graphics/GraphicsMagick \
